93fd156be6d5e356dbe60ec6598489b0077cb0a0
dkz2811 <icola@tecnomun.com.uy>
2023-08-04 16:45:58 -0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'final-diegocola/',
+  base: '/final-diegocola',
   plugins: [
     vue(),
   ],
